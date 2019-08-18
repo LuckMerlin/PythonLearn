06a8fec0526961e7348c364720d2459c33bf0c2c
luckmerlin <676046361>
2019-08-18 16:47:34 +0800

    Initial

---
+++ Test/src/com/luckmerlin/__init__.py
@@ -0,0 +1,28 @@
+#print ("sfsafasdfa")
+from _ast import If
+citys=["Chengdu","Beijing","Shanghai"]+[4353,"Sichuan"]
+print(citys[3])
+citys[3]="Yunnan"
+citys.append("Shenzhen")
+print(len(citys))
+a,b=1009,2008
+print(b)
+c=3
+if c==3:
+    print("wuyue")
+if b==2008:
+    print("sfsfasdfasf")
+jj=["woaishanghai","woaichengdu","ilove"]
+
+for  j in jj[:]:
+    print(j),len(j) 
+    if len(j)==5:
+        jj.insert(0,"beijing" )   
+print(jj[0])        
+print("beijing3" in jj)
+
+#print(x)
+
+    
+    
+    
